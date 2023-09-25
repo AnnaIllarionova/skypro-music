fc3df9791276da65636ce0fa8afbc8b122354461
AnnaIllarionova <annailinas@gmail.com>
2023-09-25 21:25:24 +0300

    added components

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ export function Navigation() {
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
-        <img className="logo__image" src="img/logo.png" alt="logo" />
+        <img className="logo__image" src="./img/logo.png" alt="logo" />
       </div>
       <div className="nav__burger burger">
         <span className="burger__line"></span>
---
+++ src/components/sidebar.js
@@ -0,0 +1,37 @@
+import { GetPersonalData } from "./personal-data.js";
+import "./sidebar.css";
+
+export function Sidebar() {
+  return (
+    <div className="main__sidebar sidebar">
+      <GetPersonalData />
+      <div className="sidebar__block">
+        <div className="sidebar__list">
+          <GetSidebarItem
+            imgUrl="./img/playlist01.png"
+            label="day's playlist"
+          />
+          <GetSidebarItem
+            imgUrl="./img/playlist02.png"
+            label="day's playlist"
+          />
+          <GetSidebarItem
+            imgUrl="./img/playlist03.png"
+            label="day's playlist"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function GetSidebarItem(props) {
+  return (
+    <div className="sidebar__item">
+      <a className="sidebar__link" href="#">
+        <img className="sidebar__img" src={props.imgUrl} alt={props.label} />
+      </a>
+    </div>
+  );
+}
+
