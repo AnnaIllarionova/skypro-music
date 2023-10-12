d64044435ad74ed37a6d0e3aa6d0d1d559a39a3a
AnnaIllarionova <annailinas@gmail.com>
2023-10-12 14:54:12 +0300

    styles for error page

--- src/pages/error-page/error-page.js
+++ src/pages/error-page/error-page.js
@@ -1,7 +1,24 @@
+import * as S from "./error-page.styled"
+
 export const ErrorPage = () => {
   return (
-    <div>
-      <p>404 Страница не существует</p>
-    </div>
+    <S.ErrorContent>
+      <S.ErrorContainer>
+        <S.ErrorNumber>404</S.ErrorNumber>
+        <S.ErrorSection>
+          <S.ErrorTitle>Страница не найдена</S.ErrorTitle>
+          <img src="./img/smile_crying.svg" alt="smile_crying" />
+        </S.ErrorSection>
+        <S.ErrorText>
+          Возможно, она была удалена
+          <br /> или перенесена на другой адрес
+        </S.ErrorText>
+        <S.ErrorBtnBackToMain>
+          <S.ErrorLinkToMain to="/">
+            Вернуться на главную
+          </S.ErrorLinkToMain>
+        </S.ErrorBtnBackToMain>
+      </S.ErrorContainer>
+    </S.ErrorContent>
   );
 };
